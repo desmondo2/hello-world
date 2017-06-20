94d187fc6322ea58f2f3ddc145caf2fea17c58c1
desmondo2 <derekthemurray@gmail.com>
2017-06-20 22:00:20 +0930

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,17 @@ function calc() {
    alert("aieee");
    var payDayIncl = 0;
    var billDay = 0;
-   payDayIncl = document.Calculator.PayDayIncl.value;
+   var margin = 0;
    
+   payDayIncl = document.Calculator.PayDayIncl.value;
+   billDay = document.Calculator.BillDay.value;
+   margin = (billDay-payDayIncl)/billDay;
+      
    alert("document.Calculator.PayDayIncl="+payDayIncl);
-   alert("document.Calculator.BillDay="+document.Calculator.BillDay);
-   document.Calculator.Margin.value = 55;
- }
+   alert("document.Calculator.BillDay="+billDay);
+
+   alert("margin="+margin);
+
+   document.Calculator.Margin.value = margin;
+ 
+}
