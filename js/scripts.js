03bf113540ea57c5b5665edc588976e97705a529
desmondo2 <derekthemurray@gmail.com>
2017-06-20 21:39:48 +0930

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 alert("Hello worlds");
 
-function Calc()
+function calc()
    alert("document.Calculator.Display="&document.Calculator.Display.value);
    document.Calculator.Margin.value = 55;
  }
